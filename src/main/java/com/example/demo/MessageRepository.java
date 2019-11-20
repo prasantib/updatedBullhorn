3faa8e7c81ef3a6286428f62b132ddba25e0e5ec
prasantib <prasanti.babu@gmail.com>
2019-11-20 16:54:59 -0500

    new boolhorn

--- src/main/java/com/example/demo/MessageRepository.java
+++ src/main/java/com/example/demo/MessageRepository.java
@@ -1,6 +1,21 @@
-
 package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface MessageRepository extends CrudRepository<Message, Long> {}
+import java.util.Map;
+
+public class MessageRepository {
+    public Object findAll() {
+    }
+
+    public Map<Object, Object> findById(long id) {
+    }
+
+    public void save(Message message) {
+    }
+
+    public void deleteById(long id) {
+    }
+
+    public interface MessageRepository extends CrudRepository<Message, Long> {}
+}
